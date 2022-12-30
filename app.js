17b5bfcbc2cd00001a671f3a3c9eed0dbe5324e2
nischaltripathi <nischaltripathi4@gmail.com>
2022-12-30 10:42:26 +1030

    able to send data to monday.com

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-import express, { json } from 'express';
-import { doWork } from './controllers/puppeteerController.js';
+import express, { json } from "express";
+import { doWork } from "./controllers/puppeteerController.js";
+import fetch from "node-fetch";
 
 const app = express();
 
-app.use(json())
+app.use(json());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
-    res.json({ status: true, message: "Our node.js app works" });
-    doWork();
+app.get("/", async (req, res) => {
+  res.json({ status: true, message: "Our node.js app works" });
 });
+app.post("/webhook/scrape_tlp", async (req, res) => {
+  var columnValues = await doWork();
+  var query = `mutation($boardId: Int!, $itemId: Int!, $columnValues: JSON!) {change_multiple_column_values (board_id: $boardId, item_id: $itemId, column_values: $columnValues) {id}}`;
 
+  const variables = {
+    boardId: req.body.payload.inboundFieldValues.boardId,
+    itemId: req.body.payload.inboundFieldValues.itemId,
+    columnValues: columnValues,
+  };
+  console.log("Column Values are as follows:");
+  console.log(columnValues);
+
+  fetch("https://api.monday.com/v2", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3ODU2NDc0MCwidWlkIjozMTA2OTg2OSwiaWFkIjoiMjAyMi0wOS0wMVQwMjo0MTowOS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6NjY4MDQ4MSwicmduIjoidXNlMSJ9.FkUIo2LrNQwetRYQ-9njWVrZsMrVNklgjqj7eldy2-o",
+    },
+    body: JSON.stringify({
+      query: query,
+      variables: variables,
+    }),
+  })
+    .then((res) => res.json({ status: "okay" }))
+    .then((res) => console.log(JSON.stringify(res, null, 2))); // req.body.payload.inboundFieldValues.boardId
+});
 app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
--- controllers/puppeteerController.js
+++ controllers/puppeteerController.js
@@ -1,55 +1,76 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
-export const doWork = () => {
-    let browser;
-    let page;
+export const doWork = async () => {
+  let page;
+  let conversations;
+  let interested;
+  let not_interested;
 
-    browser = puppeteer.launch({
-        headless: false,
-        args: ["--disable-setuid-sandbox"],
-        'ignoreHTTPSErrors': true
-    }).then(async (browser) => {
-        page = await browser.newPage();
-        await page.setViewport({
-            width: 1280,
-            height: 800,
-            isMobile: false,
-        });
-        await page.goto("https://huddleco.app/cloud/login", {
-            waitUntil: "networkidle2",
-        });
-        await page.waitForSelector('input[name=email]');
-        await page.type('input[name=email]', 'Huddle123@tlp', {});
-        await page.waitForSelector('input[name=password]');
-        await page.type('input[name=password]', 'Huddle2TLP!', {});
-
-        await page.waitForSelector(".login100-form-btn");
-        await page.click(".login100-form-btn");
-        await page.waitForNavigation({
-            waitUntil: 'networkidle0',
-        });
-
-        await page.waitForSelector(".nav-item");
-        await page.goto("https://huddleco.app/cloud/inbox", { waitUntil: 'domcontentloaded' });
-        await page.click("#ractive > div.content > div.page-inbox > div.page-inbox-header > div:nth-child(3)");
-
-        await page.waitForTimeout(10000);
-        const conversations = await page.evaluate(() => {
-            return document.querySelector("#ractive > div.content > div.page-inbox > div.page-inbox-header > div:nth-child(3) > div > a.dropdown-item.active > div").textContent;
-        });
-        const interested = await page.evaluate(() => {
-            return document.querySelector("#ractive > div.content > div.page-inbox > div.page-inbox-header > div.dropdown.ml-3.show > div > a:nth-child(3) > div").textContent;
-        });
-        const not_interested = await page.evaluate(() => {
-            return document.querySelector("#ractive > div.content > div.page-inbox > div.page-inbox-header > div.dropdown.ml-3.show > div > a:nth-child(4) > div").textContent;
-        });
-
-        console.log(conversations);
-        console.log(interested);
-        console.log(not_interested);
-
-
-    }).catch((err) => {
-        console.log("Could not create a browser instance => : ", err);
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--disable-setuid-sandbox"],
+      ignoreHTTPSErrors: true,
     });
+    page = await browser.newPage();
+    await page.setViewport({
+      width: 1280,
+      height: 800,
+      isMobile: false,
+    });
+    await page.goto("https://huddleco.app/cloud/login", {
+      waitUntil: "networkidle2",
+    });
+    await page.waitForSelector("input[name=email]");
+    await page.type("input[name=email]", "Huddle123@tlp", {});
+    await page.waitForSelector("input[name=password]");
+    await page.type("input[name=password]", "Huddle2TLP!", {});
+
+    await page.waitForSelector(".login100-form-btn");
+    await page.click(".login100-form-btn");
+    await page.waitForNavigation({
+      waitUntil: "networkidle0",
+    });
+
+    await page.waitForSelector(".nav-item");
+    await page.goto("https://huddleco.app/cloud/inbox", {
+      waitUntil: "domcontentloaded",
+    });
+    await page.click(
+      "#ractive > div.content > div.page-inbox > div.page-inbox-header > div:nth-child(3)"
+    );
+
+    await page.waitForTimeout(10000);
+
+    conversations = await page.evaluate(() => {
+      return document
+        .querySelector(
+          "#ractive > div.content > div.page-inbox > div.page-inbox-header > div:nth-child(3) > div > a.dropdown-item.active > div"
+        )
+        .textContent.replace(" conversations", "");
+    });
+    interested = await page.evaluate(() => {
+      return document
+        .querySelector(
+          "#ractive > div.content > div.page-inbox > div.page-inbox-header > div.dropdown.ml-3.show > div > a:nth-child(3) > div"
+        )
+        .textContent.replace(" conversations", "");
+    });
+
+    not_interested = await page.evaluate(() => {
+      return document
+        .querySelector(
+          "#ractive > div.content > div.page-inbox > div.page-inbox-header > div.dropdown.ml-3.show > div > a:nth-child(4) > div"
+        )
+        .textContent.replace(" conversations", "");
+    });
+
+    return JSON.stringify({
+      dup__of_profiles: conversations,
+      dup__of_conversations: interested,
+      dup__of_interested: not_interested,
+    });
+  } catch (err) {
+    console.log("Could not create a browser instance => : ", err);
+  }
 };
